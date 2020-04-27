dde753f38f8bc37e4b65e93b6cdc57e7ba4438f2
DemonLord98 <ahmedalkadhim20001@gmail.com>
2020-04-27 15:30:09 +0300

    Fixed some issues, added some comments

--- SatCal/SatCal.py
+++ SatCal/SatCal.py
@@ -1,24 +1,27 @@
 import math
 
-EarthRad = 6378.14
-GeoRad = 42164.17
-GeoAlt = 35786
-EccOfEarth = 0.08182
+EarthRad = 6378.14          # Earth raduis on the equator
+GeoRad = 42164.17           # Geostationary orbit raduis 
+GeoAlt = 35786              # GSO altitude
+EccOfEarth = 0.08182        # Eccentricity of the Earth
 
 EarthStaLat = math.radians(float(input("Input your latitude in degrees: ")))
 EarthStaLon = math.radians(float(input("Input your longitude in degrees: ")))
 EarthStaAlt = float(input("Input your altitude in Kilometers: "))
-SatStaAlt = math.radians(float(input("Input the satellite longitude: ")))
+SatStaLon = math.radians(float(input("Input the satellite longitude: ")))
 
 
 l = ((EarthRad / math.sqrt(1 - pow(EccOfEarth, 2) * pow(math.sin(EarthStaLat), 2))) + EarthStaAlt) * math.cos(EarthStaLat)
 z = ((EarthRad * (1 - pow(EccOfEarth, 2)) / math.sqrt(1 - pow(EccOfEarth, 2) * pow(math.sin(EarthStaLat), 2))) + EarthStaAlt) * math.sin(EarthStaLat)
 
-R = math.sqrt(l ** 2 + z ** 2)
-InterAngle = math.atanh(z/ l)
-B = EarthStaLon - SatStaAlt
-d = math.sqrt(pow(R, 2) + pow(GeoRad, 2) - 2 * R * GeoRad * math.cos(InterAngle) *  math.cos(B))
+# Calculate the Raduis to the center of the Earth from the ground station
+StaRad = math.sqrt(l ** 2 + z ** 2)
+# Calculate the intermediate angle ΨE
+InterAngle = math.tanh(z/ l)
+B = EarthStaLon - SatStaLon
+# Calculate the Range to the satellite
+d = math.sqrt(pow(StaRad, 2) + pow(GeoRad, 2) - 2 * StaRad * GeoRad * math.cos(InterAngle) *  math.cos(B))
 
-print ("\nRaduis to the center of the Earth from the ground station:\n\tR = ",R)
-print ("\nThe intermediate angle Ψ:\n\tΨ = ",InterAngle)
-print ("\nThe distance to the satellite:\n\td = ",d)
+
+print ("\nThe Range/distance to the satellite:\n\td = ",d)
+
